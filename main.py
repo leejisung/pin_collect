b200ff3021e36912273c2f6c27f6a9d387c13040
leejisung <starseed13@hanmail.net>
2019-08-08 21:31:19 +0900

    9:31

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import urllib.request
+import time
 import random
 from selenium import webdriver
 image_count = 5
+#juso = 'https://www.pinterest.co.kr/mornthadtartee/%E0%B8%AD%E0%B8%B0%E0%B8%99%E0%B9%80%E0%B8%A1%E0%B8%B0/'
 juso = 'https://www.pinterest.co.kr/starseed13/%EB%AA%A8%EC%97%90/'
-
 
 driver = webdriver.Chrome('chromedriver')
 driver.get(juso)
+
+title_ = driver.title
+for i in range(len(title_)):
+    if title_[i]== '이' and title_[i+1]== '미' and title_[i+2]== '지':
+        number_start = i+4
+        break
+pin_number = ''
+
+for i in range(number_start, len(title_)):
+    if title_[i]!= '개':
+        pin_number+=title_[i]
+    else:
+        break
+pin_number = int(pin_number)
+
+for i in range(5):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+
 images = driver.find_elements_by_tag_name('img')
 len_img = len(images)
                
@@ -17,9 +37,9 @@ for image in images:
 del images[0]
 
     
-num_images = [random.randint(0,len_img)]
+num_images = [random.randint(1,len_img)]
 while len(num_images)!=image_count:
-    rand=random.randint(0,len_img)
+    rand=random.randint(1,len_img)
     for i in(range(len(num_images))):        
         if rand == num_images[i]:
             break
