379ce5e02d8c64793d3d19c421c071824df80e54
leejisung <starseed13@hanmail.net>
2019-08-09 13:50:44 +0900

    8월9일 1시 51분

---
+++ setting.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import pandas as pd
+
+
+root = Tk()
+
+data = pd.read_csv('data.csv', sep=',')
+
+
+def click():
+    juso = txt.get()
+    gr = txt2.get()
+    
+    global data
+    data.site[0] = juso
+    data.local[0] = gr
+ 
+lbl = Label(root, text="주소")
+lbl.grid(row=0, column=0)
+txt = Entry(root)
+txt.grid(row=0, column=1)
+
+lbl2 = Label(root, text="경로")
+lbl2.grid(row=1, column=0)
+txt2 = Entry(root)
+txt2.grid(row=1, column=1)
+ 
+btn = Button(root, text="OK", command = click)
+btn.grid(row=2, column=1)
+ 
+root.mainloop()
+
